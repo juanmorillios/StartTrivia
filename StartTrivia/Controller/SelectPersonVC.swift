54d7c7585baf776715a0b90b753c72ffc846aa5c
Juan Morillo <juanmorillios@gmail.com>
2018-10-11 17:38:33 +0200

    Setting MainView

--- StartTrivia/Controller/SelectPersonVC.swift
+++ StartTrivia/Controller/SelectPersonVC.swift
@@ -9,10 +9,11 @@
 import UIKit
 
 class SelectPersonVC: UIViewController {
-
+  @IBOutlet weak var stackBg: UIView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    stackBg.layer.cornerRadius = 10
   }
 
 
