694821d0da24600e5697a5dc4ee6a6bebc72035f
Juan Morillo <juanmorillios@gmail.com>
2018-10-11 18:13:16 +0200

    Remove code and create class with constants

--- StartTrivia/Controller/SelectPersonVC.swift
+++ StartTrivia/Controller/SelectPersonVC.swift
@@ -9,11 +9,10 @@
 import UIKit
 
 class SelectPersonVC: UIViewController {
-  @IBOutlet weak var stackBg: UIView!
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    stackBg.layer.cornerRadius = 10
+    
   }
 
 
